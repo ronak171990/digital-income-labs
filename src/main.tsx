d9a96181e1466e8f7fa779c876afdda1c2a2b66d
Ronak Ambu Patel <ronakpatel171990@gmail.com>
2026-06-21 14:04:25 +0530

    Added Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4';
+
 import App from './App.tsx';
 import './index.css';
+
+ReactGA.initialize('G-7J99GB543W');
+
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
